cc65383f40eb86cf73ac01d77bca31e38d4343c4
mf <marco@fotino.it>
2012-07-25 21:54:52 +0000

    restoring some files...

---
+++ fot1-java/src/test/java/it/fotino/java/io/FileOperationsTest.java
@@ -0,0 +1,19 @@
+package it.fotino.java.io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileOperationsTest {
+
+	@Test
+	public void testReadFileToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteStringToFile() {
+		fail("Not yet implemented");
+	}
+
+}
